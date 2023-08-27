6f70fc795c92c088a1b3996b3537d65dde9be251
VladaHrazhdiian <123592785+VladaHrazhdiian@users.noreply.github.com>
2023-08-27 16:19:44 +0300

    work

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,7 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Notify } from 'notiflix';
+import './App.css';
+
+import ContactForm from './ContactForm';
+import ContactList from './ContactList';
+import Filtr from './Filter';
